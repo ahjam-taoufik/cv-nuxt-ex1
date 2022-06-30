b5ee137669a39882e57e258c8a692f057ff1f931
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-30 14:48:48 +0200

    pass params in route , nuxt link , route programatic

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <nuxt-link to="/users">go to users</nuxt-link>
+        <p>
+            user Id : {{$route.params.id}}
+        </p>
+    </div>
+</template>
---
+++ pages/users/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>index if all users</p>
+        <input v-model="user" />
+        <button v-on:click="getUser">get</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            user:''
+        }
+    },
+    methods: {
+        getUser:function(){
+            this.$router.push('/users/'+this.user)
+        }
+    }
+}
+</script>
