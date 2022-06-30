06104ed9bf576eb77eb6e6fc06390b12ac6a51cb
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-30 16:29:41 +0200

    custom error page

---
+++ layouts/customError.vue
@@ -0,0 +1,8 @@
+<template>
+ <div>
+    <h1>custom Error page Header</h1>
+  <Nuxt />
+    <h1>custom Error page Footer</h1>
+   
+ </div>
+</template>
--- layouts/error.vue
+++ layouts/error.vue
@@ -10,6 +10,6 @@
 <script>
 export default {
   props: ['error'],
-  layout: 'blog' // you can set a custom layout for the error page
+  layout: 'customError' // you can set a custom layout for the error page
 }
 </script>
